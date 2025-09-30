fc5704a08f4698a7e55a470f2ff569677c5df513
Vinayak-Gawade <vinayakgawade35@gmail.com>
2025-09-30 21:56:09 +0530

    Initial resume site setup

---
+++ assets/js/main.js
@@ -0,0 +1,11 @@
+// small helpers
+document.querySelectorAll('a[href^="#"]').forEach(a=>{
+  a.addEventListener('click', e=>{
+    const href = a.getAttribute('href');
+    if(href.length>1){
+      e.preventDefault();
+      const el = document.querySelector(href);
+      if(el) el.scrollIntoView({behavior:'smooth', block:'start'});
+    }
+  })
+});
